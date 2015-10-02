78ac7fef01af426b37b49f888ad1c81dbc53ffaa
gregr <greg.weiqi@gmail.com>
2015-10-02 09:51:07 -0400

    Define chi_squared_prob

--- data.py
+++ data.py
@@ -1,4 +1,5 @@
 from __future__ import absolute_import
+from bisect import bisect_left
 import csv
 from itertools import chain, izip_longest
 from .dict import countdict, countdict_setops
@@ -267,6 +268,28 @@ def chi_squared(pair_freqs):
 
 def chi_squared_with(*args, **kwargs):
     return chi_squared(pairwise_freqs(*args, **kwargs))
+
+confidences = (1.0, 0.95, 0.9, 0.8, 0.7, 0.5, 0.3, 0.2, 0.1, 0.05, 0.01, 0.001)
+
+chi_squareds = [
+    (0.004, 0.02, 0.06, 0.15, 0.46, 1.07, 1.64, 2.71, 3.84, 6.64, 10.83),
+    (0.10, 0.21, 0.45, 0.71, 1.39, 2.41, 3.22, 4.60, 5.99, 9.21, 13.82),
+    (0.35, 0.58, 1.01, 1.42, 2.37, 3.66, 4.64, 6.25, 7.82, 11.34, 16.27),
+    (0.71, 1.06, 1.65, 2.20, 3.36, 4.88, 5.99, 7.78, 9.49, 13.28, 18.47),
+    (1.14, 1.61, 2.34, 3.00, 4.35, 6.06, 7.29, 9.24, 11.07, 15.09, 20.52),
+    (1.63, 2.20, 3.07, 3.83, 5.35, 7.23, 8.56, 10.64, 12.59, 16.81, 22.46),
+    (2.17, 2.83, 3.82, 4.67, 6.35, 8.38, 9.80, 12.02, 14.07, 18.48, 24.32),
+    (2.73, 3.49, 4.59, 5.53, 7.34, 9.52, 11.03, 13.36, 15.51, 20.09, 26.12),
+    (3.32, 4.17, 5.38, 6.39, 8.34, 10.66, 12.24, 14.68, 16.92, 21.67, 27.88),
+    (3.94, 4.87, 6.18, 7.27, 9.34, 11.78, 13.44, 15.99, 18.31, 23.21, 29.59)]
+
+
+def chi_squared_prob(chi_squared, deg_freedom):
+    if deg_freedom < 1 or deg_freedom > 10:
+        return None
+    scores = chi_squareds[deg_freedom - 1]
+    idx = bisect_left(scores, chi_squared, 0, len(scores))
+    return confidences[idx]
 
 
 def transformed_row(transforms, row):
